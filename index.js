a830978e843348f9e511b35538f69963eef80299
Thomas Bernal <thhomasgt99@gmail.com>
2024-06-02 14:04:31 -0500

    mongodb

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 //conexion database
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/tecnopac')
+mongoose.connect('mongodb://localhost:27017/tecnopac')
 	.then(()=>{
 		console.log("La conexion a la base de datos se ha realizado correctamente")
 
